a48d00ad2844ba13fa640c3e2c25c0d54d2d7c03
Frank Liang <frank97liang@gmail.com>
2018-01-31 00:21:48 -0500

    added plotly and chart component

---
+++ app_ui/src/components/StockGraph.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Plotly from 'plotly.js/dist/plotly';
+
+export default class StockGraph extends React.Component {
+    constructor(props){
+        super(props);
+
+
+    }
+
+    render(){
+        let graphDiv = document.getElementById('graph');
+        
+        if(graphDiv !== null){
+            Plotly.newPlot(graphDiv,
+                [{
+                    type: 'scatter',
+                    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    y: [2, 4, 1, 8, 12, 21, 2, 4, 12, 31]
+                }],
+                {
+                    margin: { t: 20, b: 80, l: 70, r: 70},
+                    showlegend: false,
+                    paper_bgcolor: '#152935',
+                    plot_bgcolor: '#152935',
+                    xaxis: {
+                        color: '#FFFFFF'
+                    },
+                    yaxis: {
+                        color: '#FFFFFF'
+                    }
+                }
+            );
+            window.addEventListener("resize", function() { Plotly.Plots.resize(graphDiv)} );
+        }
+        
+        return(
+            <div style={{backgroundColor: '#152935'}}>
+                <div id="graph" style={{width: '60%', height: '100%'}}></div>
+            </div>
+        )
+    }
+}
--- app_ui/src/containers/HomePage.jsx
+++ app_ui/src/containers/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StockGraph from '../components/StockGraph.jsx';
 
 class HomePage extends React.Component{
     constructor(props){
@@ -7,8 +8,11 @@ class HomePage extends React.Component{
 
     render(){
         return(
-            <div>
-                Hello World!
+            <div className='container' style={{width: '100', backgroundColor: '#1C2D35'}}>
+                <div style={{height: '30px', fontSize: '30px', color: 'white', paddingBottom: '25px'}}>
+                    Real Time Visualization
+                </div>
+                <StockGraph />
             </div>
         )
     }
